c730181507726a309f837e598d713b31013502de
Martin Peck <mpeck@codeclub.org.uk>
2015-06-24 12:47:32 +0100

    extract the keyword_link if it exists

--- export.py
+++ export.py
@@ -12,11 +12,11 @@ import urllib
 def main(argv=None):
     """
     This script exports all Bit.ly urls for an account.
-    
+
     Required options:
         -l=, --login=: Bit.ly login
         -p=, --password=: Bit.ly Password (used to generate OAuth token)
-    
+
     Optional parameters:
         -v: Verbose mode
         -h: Display this help and exit
@@ -25,7 +25,7 @@ def main(argv=None):
     # Load options using getopt
     if argv is None:
         argv = sys.argv
-    
+
     try:
         opts, args = getopt.getopt(
             argv[1:],
@@ -35,14 +35,14 @@ def main(argv=None):
     except getopt.error, msg:
         print "Option parsing error: %s" % str(msg)
         return 2
-    
+
     # Setup defaults
     verbose = False
     login = None
     password = None
     user = None
     output_path = 'links.csv'
-    
+
     try:
         for option, value in opts:
             if option == "-v":
@@ -64,43 +64,44 @@ def main(argv=None):
         print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err)
         print >> sys.stderr, "\t for help use --help"
         return 2
-    
+
     if login is None:
         raise Exception('Login parameter must be present.')
-    
+
     if password is None:
         raise Exception('Password parameter must be present.')
-    
+
     bitly = Bitly(login, password, verbose)
-    
+
     limit = 100
     offset = 0
     result_count = 0
     nb_found = 0
-    
+
     csv_writer = csv.writer(open(output_path, 'wb'), quoting=csv.QUOTE_ALL)
     csv_writer.writerow(('Link', 'Long url'))
-    
+
     while offset <= result_count:
         data = bitly.user_link_history(limit=limit, offset=offset, user=user)
-        
+
         result_count = data['result_count']
-        
+
         for link in data['link_history']:
-            csv_writer.writerow((link['link'], link['long_url'].encode('utf8')))
-            
+            print link
+            csv_writer.writerow((link['link'], link.get('keyword_link', '').encode('utf8'), link['long_url'].encode('utf8')))
+
             nb_found += 1
-        
+
         offset += limit
-        
+
         if verbose:
             progress = float(offset) / result_count
-            
+
             sys.stdout.write("\r(%2d%%) Loaded %5d/%5d links..." % (
                 round(progress * 100), offset, result_count
             ))
             sys.stdout.flush()
-    
+
     if verbose:
         print ""
         print "Done! Found %d links, expected %d." % (nb_found, result_count)
@@ -110,26 +111,26 @@ class Bitly(object):
         super(Bitly, self).__init__()
         self.login = login
         self.verbose = verbose
-        
+
         self.access_token = requests.post(
             'https://api-ssl.bitly.com/oauth/access_token',
             auth=(login, password),
             timeout=10
         ).text
-        
+
         if verbose:
             print "Access token retrieved: %s" % self.access_token
-        
+
     def user_link_history(self, limit=50, offset=0, user=None):
         params = {
             'limit': limit,
             'offset': offset,
             'archived': 'both'
         }
-    
+
         if user is not None:
             params['user'] = user
-    
+
         return self._call('v3/user/link_history', params)['data']
 
     def _call(self, method, params):
@@ -139,9 +140,9 @@ class Bitly(object):
         """
         # default to json
         params['format'] = params.get('format','json')
-        
+
         params['access_token'] = self.access_token
-        
+
         # force to utf8 to fix ascii codec errors
         encoded_params = []
         for k,v in params.items():
@@ -151,22 +152,22 @@ class Bitly(object):
                 v = str(v).encode('UTF8')
             encoded_params.append((k,v))
         params = dict(encoded_params)
-        
+
         request = "https://api-ssl.bitly.com/%(method)s?%(params)s" % {
             'method': method,
             'params': urllib.urlencode(params, doseq=True)
         }
-        
+
         http_response = requests.get(request, timeout=10)
-        
+
         if http_response.status_code != 200:
             raise Exception('HTTP %d Error: %s' % (http_response.status_code, http_response.text))
-        
+
         data = http_response.json()
-        
+
         if data is None or data.get('status_code', 500) != 200:
             raise Exception('Bitly returned error code %d: %s' % (data.get('status_code', 500), data.get('status_txt', 'UNKNOWN_ERROR')))
-        
+
         return data
 
 if __name__ == '__main__':
